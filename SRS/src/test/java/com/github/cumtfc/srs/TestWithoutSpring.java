e9d04b6ce35ca790e23fee4c95d2a26c2952008f
fc <fc751590330@outlook.com>
2018-06-25 21:19:23 +0800

    update

--- SRS/src/test/java/com/github/cumtfc/srs/TestWithoutSpring.java
+++ SRS/src/test/java/com/github/cumtfc/srs/TestWithoutSpring.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.cumtfc.srs.po.course.Course;
+import sun.applet.Main;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +20,25 @@ import java.util.List;
 public class TestWithoutSpring {
 
     public static void main(String[] args) {
+        TestWithoutSpring main = new TestWithoutSpring();
+        main.testAll();
+    }
+
+    private void testAll(){
+        for (Method method : this.getClass().getDeclaredMethods()) {
+            if (method.getParameterCount()>0) {
+                System.out.println("测试方法必须无参");
+                continue;
+            }
+            try {
+                method.invoke(this);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void testJackson(){
         ObjectMapper mapper = new ObjectMapper();
         List<Course> list = new ArrayList<>();
         Course course = new Course();
